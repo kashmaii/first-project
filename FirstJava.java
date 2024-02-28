5d95f53685ea6271ec258cddba246b277063e7cd
kashmaii <160463637+kashmaii@users.noreply.github.com>
2024-02-27 20:21:00 -0800

    Update FirstJava.java

--- FirstJava.java
+++ FirstJava.java
@@ -1,7 +1,7 @@
  public class FirstJava
     {
-      public sttaic void main(String[] args)
+      public static void main(String[] args)
        {
-         System.out.pritnln("Hello! Welcome to Jenkins!") ;
+         System.out.println("Hello! Welcome to Jenkins!") ;
        }
     }
